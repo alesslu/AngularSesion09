12b14cad433666e85963037a391dc038fc808287
alesslu <alessluchata@gmail.com>
2023-01-02 19:16:58 -0500

    resolucion

--- y/src/app/calculadora/calculadora.component.ts
+++ y/src/app/calculadora/calculadora.component.ts
@@ -6,5 +6,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./calculadora.component.css']
 })
 export class CalculadoraComponent {
+  valoruno: number = 0;
+  valordos: number = 0;
+  resultado: number = 0;
+  operacion: string = 'defecto';
+
+  onSumar(): void {
+    this.resultado = this.valoruno + this.valordos;
+  }
+  onRestar(): void {
+    this.resultado = this.valoruno - this.valordos;
+  }
+  onMultiplicar(): void {
+    this.resultado = this.valoruno * this.valordos;
+  }
+  onDividir(): void {
+    this.resultado = this.valoruno / this.valordos;
+  }
+
+
+  calcular(): number {
+    if (this.operacion === 'sumar') {
+      this.onSumar();
+      return this.resultado;
+    } else if (this.operacion === 'restar') {
+      this.onRestar();
+      return this.resultado;
+    } else if (this.operacion === 'multiplicar') {
+      this.onMultiplicar();
+      return this.resultado;
+    } else if (this.operacion === 'dividir') {
+      this.onDividir();
+      return this.resultado;
+    }
+
+    return 0;
+  }
 
 }
